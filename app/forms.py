cd844b2d1b894a20c9adc7b02b494f7fb463d725
Jit <prithvirajpramanik@yahoo.co.in>
2016-01-13 16:24:18 +0530

    Added form

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,7 @@
-from flask.ex.wtf import Form 
-from wt.forms import 
+from flask.ext.wtf import Form 
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+	openid= StringField('openid', validators=[DataRequired()])
+	remember_me=BooleanField('remember_me',default=False)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, flash , redirect
 from app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form=LoginForm()
+	return render_template('login.html',title='Sign In',form=form)
+
+
 def index():
 	user = {'nickname':'Miguel'}  #fake user
 	post= [ #array of post
