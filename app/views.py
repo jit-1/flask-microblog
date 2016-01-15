680ea05aa9af7f0a71717efad918ac22a0caf43f
Jit <prithvirajpramanik@yahoo.co.in>
2016-01-15 20:15:40 +0530

    Added error message output

--- app/views.py
+++ app/views.py
@@ -3,14 +3,23 @@ from app import app
 from .forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+""""@app.route('/')"""
+@app.route('/index)
 @app.route('/login',methods=['GET','POST'])
+
 def login():
 	form=LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s",remember_me=%s' % (form.openid.data,str(form.remember_me.data)))
+		return redirect('/index')
 	return render_template('login.html',title='Sign In',form=form)
 
 
+
+
+
+
+"""
 def index():
 	user = {'nickname':'Miguel'}  #fake user
 	post= [ #array of post
@@ -28,3 +37,4 @@ def index():
 	},
 	]
 	return render_template('index.html',title='Home',user=user,posts=post)
+"""
