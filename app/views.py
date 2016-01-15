c56d480573854319c4d47345b4c15f3922cd3d43
Jit <prithvirajpramanik@yahoo.co.in>
2016-01-15 20:41:47 +0530

    Added OpenID sources

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ from .forms import LoginForm
 
 
 """"@app.route('/')"""
-@app.route('/index)
+
+@app.route('/index')
 @app.route('/login',methods=['GET','POST'])
 
 def login():
@@ -12,7 +13,7 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for OpenID="%s",remember_me=%s' % (form.openid.data,str(form.remember_me.data)))
 		return redirect('/index')
-	return render_template('login.html',title='Sign In',form=form)
+	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
 
 
 
